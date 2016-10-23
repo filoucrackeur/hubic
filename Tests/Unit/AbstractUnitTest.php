8a4daa2e1d8a0374229321f2414611e8d485775d
Philippe COURT <philippe.court@gmail.com>
2016-10-23 13:28:33 +0200

    [BUGFIX] travis

---
+++ Tests/Unit/AbstractUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Filoucrackeur\Hubic\Tests\Unit;
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+abstract class AbstractUnitTest extends UnitTestCase
+{
+    protected function setUp()
+    {
+
+    }
+
+}
