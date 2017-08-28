2762d686d5d2d65372887a74f7ab8e776f1988c7
Philippe Court <philippe.court@gmail.com>
2017-08-29 00:18:01 +0200

    [CLEAN] model class

--- Configuration/TCA/tx_hubic_domain_model_account.php
+++ Configuration/TCA/tx_hubic_domain_model_account.php
@@ -4,57 +4,57 @@ defined('TYPO3_MODE') or die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_hubic_domain_model_account');
 
 return [
-    'ctrl' => array(
+    'ctrl' => [
         'title' => 'LLL:EXT:hubic/Resources/Private/Language/locallang_db.xlf:tx_hubic_domain_model_account',
         'label' => 'name',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'languageField' => 'sys_language_uid',
         'delete' => 'deleted',
-        'enablecolumns' => array(
+        'enablecolumns' => [
             'disabled' => 'hidden'
-        ),
+        ],
         'iconfile' => 'EXT:hubic/Resources/Public/Icons/icon_tx_hubic_domain_model_account.gif'
-    ),
-    'interface' => array(
+    ],
+    'interface' => [
         'showRecordFieldList' => 'name,client_id,client_secret,access_token'
-    ),
-    'types' => array(
-        '0' => array('showitem' => 'hidden,name,client_id,client_secret,access_token'),
-        '1' => array('showitem' => 'hidden,name,client_id,client_secret,access_token')
-    ),
-    'palettes' => array(),
-    'columns' => array(
-        'name' => array(
+    ],
+    'types' => [
+        '0' => ['showitem' => 'hidden,name,client_id,client_secret,access_token'],
+        '1' => ['showitem' => 'hidden,name,client_id,client_secret,access_token']
+    ],
+    'palettes' => [],
+    'columns' => [
+        'name' => [
             'label' => 'Name',
-            'config' => array(
+            'config' => [
                 'type' => 'input',
                 'size' => 40,
                 'eval' => 'trim,required'
-            )
-        ),
-        'client_id' => array(
+            ]
+        ],
+        'client_id' => [
             'label' => 'Client ID',
-            'config' => array(
+            'config' => [
                 'type' => 'input',
                 'size' => 40,
                 'eval' => 'trim,required'
-            )),
-        'client_secret' => array(
+            ]],
+        'client_secret' => [
             'label' => 'Client Secret',
-            'config' => array(
+            'config' => [
                 'type' => 'input',
                 'size' => 40,
                 'eval' => 'trim,required'
-            )),
-        'access_token' => array(
+            ]],
+        'access_token' => [
             'label' => 'Access Token',
-            'config' => array(
+            'config' => [
                 'type' => 'input',
                 'size' => 40,
                 'readOnly' => 1,
                 'default' => '',
                 'eval' => 'trim'
-            )),
-    )
+            ]],
+    ]
 ];
