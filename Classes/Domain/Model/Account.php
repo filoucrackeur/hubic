318b900f6f69f18b61c475725985dac4c989aa9e
Philippe Court <philippe.court@gmail.com>
2018-03-18 23:55:38 +0100

    [BUGFIX] getExpirationDate() must be an instance of DateTime, null returned

--- Classes/Domain/Model/Account.php
+++ Classes/Domain/Model/Account.php
@@ -155,7 +155,7 @@ class Account extends AbstractEntity
     /**
      * @return \DateTime|null
      */
-    public function getExpirationDate(): \DateTime
+    public function getExpirationDate()
     {
         return $this->expirationDate;
     }
